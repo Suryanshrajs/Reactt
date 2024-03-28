4c578c016349917d9d55224db989acfd9f484e4c
Suryanshrajs <suryansh.rajs96@gmail.com>
2024-03-28 12:47:51 +0530

    .

--- myapp/src/component/App.js
+++ myapp/src/component/App.js
@@ -1,30 +1,51 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState} from 'react';
+import './App.css'
+
 export default function App() {
-const[tasks,setTask]=useState([
+  // const[count,setCount]=useState(10)
+  // function add(){
+  //   setCount(count+1)
+  // }
+  // function sub(){
+  //   count=count-1;
+  //   console.log(count);
+  // }
+  // return (
+  //   <>
+  //   <h1>Total number : {count}</h1>
+  //   <button onClick={add}>ADD</button>
+  //   {/* <button onClick={sub}>SUB</button> */}
+  //   </>
+  // )
+
+
+  // const tasks=[
+  //   {id:1,name:"This is task 1",complete:true},
+  //   {id:2,name:"This is task 2",complete:false},
+  //   {id:3,name:"This is task 3",complete:true}
+  // ]
+
+  const[tasks,setTask]=useState([
     {id:1,name:"This is task 1",complete:true},
     {id:2,name:"This is task 2",complete:false},
-    {id:3,name:"This is task 3",complete:true},
-])
-function taskdelete(id)
-{
-setTask(tasks.filter((t)=>t.id!=id))
-}
-return(
-<div className="App">
-    <h1>Task-List</h1>
-    <ul>
-{tasks.map((task)=><li>{task.id}--------{task.name}
+    {id:3,name:"This is task 3",complete:true}
+  ])
 
-<button onClick={()=>taskdelete(task.id)}>Delete</button>
+  function taskDelete(id){
+    setTask(tasks.filter((t)=>t.id!=id))
+    
+  }
 
-</li>    )}
-
-    </ul>
-</div>
-
-
-)
-
- 
+  return(
+    <div className="App">
+      <h1>Task-List</h1>
+      <button className='btn'>Show/Hide</button>
+      <ul>
+      {tasks.map((task)=><li>{task.id}---------{task.name}
+      <button onClick={()=>taskDelete(task.id)} className='btn deletebtn'>Delete</button>
+      </li> )}
+      </ul>
+    </div>
+  )
 }
